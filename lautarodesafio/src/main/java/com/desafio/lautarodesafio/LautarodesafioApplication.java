e6b7b24bf8a2484b2535ca54415a9d2474b71426
Lautaro Vergara Amodeo <lautarovergaramodeo@gmail.com>
2023-02-06 17:25:58 -0300

    Primer commit

---
+++ lautarodesafio/src/main/java/com/desafio/lautarodesafio/LautarodesafioApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.lautarodesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LautarodesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LautarodesafioApplication.class, args);
+	}
+
+}
